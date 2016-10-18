465b991f4183182d09810542907e0dbc6b06cdce
Tyler <tcameronsmith@gmail.com>
2016-10-18 15:11:52 -0400

    take name and calculate year born

--- exercise3.rb
+++ exercise3.rb
@@ -2,3 +2,14 @@ puts "What is your name?"
 name = gets.chomp
 
 puts "Hi #{name}"
+
+puts "What is your age?"
+age = gets.chomp
+
+year = 2016 - age.to_i
+
+if year >= 1986
+  puts "wow! #{year}, you're young!"
+else
+  puts "wow! #{year}, you're old!"
+end
