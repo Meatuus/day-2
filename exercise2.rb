10ebcb2017526227d5565003242bfeb82ea04493
Tyler <tcameronsmith@gmail.com>
2016-10-18 14:51:45 -0400

    exercise 2 complete

---
+++ exercise2.rb
@@ -0,0 +1,11 @@
+# 1. calculate tax on 55
+puts 55 * 0.13
+
+# 2. adding a string with an integer
+puts "Hello" + " " + 100.to_s
+
+# 3. output a result using string interpolation
+puts "the answer to this is #{45628 * 7839}"
+
+# figuring out boolean equations
+puts (10 < 20 && 30 < 20) || !(10 == 11)
